53a81b67f2936c1281616d71c1b4440063a7cb92
Victor_Barzana <Victor_Barzana@swissre.com>
2020-11-17 17:19:35 +0200

    feature: adding first draft of the event-details component to show all details of events

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-event-details',
-  templateUrl: './event-details.component.html',
-  styleUrls: ['./event-details.component.css']
+    selector: 'app-event-details',
+    templateUrl: './event-details.component.html',
+    styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
+    event: any;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
