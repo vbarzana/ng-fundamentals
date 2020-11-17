2bcca9bd75148be503d11eb6bd16ed563fce8d17
Victor_Barzana <Victor_Barzana@swissre.com>
2020-11-17 11:40:18 +0200

    refactor: adding images folder and renaming the app

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    template: '<h1>Hi there</h1>',
+    styleUrls: []
+})
+export class EventsAppComponent {
+    title = 'ng-fundamentals';
+}
