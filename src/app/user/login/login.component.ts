cd930562dc312b373d127bb3e96e5b13cd8eb297
Victor_Barzana <Victor_Barzana@swissre.com>
2020-11-20 17:40:24 +0200

    fix: wiring up the cancel button

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
 
 @Component({
     templateUrl: './login.component.html',
@@ -8,10 +9,15 @@ export class LoginComponent {
     private userName: string;
     private password: string;
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private router: Router) {
     }
 
-    public login(formValues) {
+    login(formValues) {
         this.authService.loginWithUserAndPassword(formValues.userName, formValues.password);
+        this.router.navigate(['events']);
+    }
+
+    cancel() {
+        this.router.navigate(['events']);
     }
 }
